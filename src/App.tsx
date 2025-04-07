130bf346b0f1c964499e2da9eeaaa68f70a4224b
serezha <scvrdio@gmail.com>
2025-04-07 22:16:40 +0300

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import Home from './pages/Home';
 export default function App() {
   useEffect(() => {
     WebApp.ready();
+    WebApp.expand();
   }, []);
 
   return (
     <div className="min-h-screen bg-black flex justify-center">
-      <div className="w-[400px] bg-black text-white min-h-screen">
+      <div className="w-full max-w-[400px] bg-black text-white min-h-screen">
         <Home />
       </div>
     </div>
