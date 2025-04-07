23b2eec61ba18135d7f4b063687a9d4514aa6972
serezha <scvrdio@gmail.com>
2025-04-08 01:10:45 +0300

    update11

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Magnifier, NutHex } from '@gravity-ui/icons';
 
 export default function Header() {
   return (
-    <div style={{ paddingTop: 'env(safe-area-inset-top)' }}>
+    <div className="pt-14">
       <div className="flex items-center rounded-xl px-6 py-2">
         {/* Лупа слева */}
         <Magnifier className="text-gray-400 mr-3" />
@@ -15,7 +15,7 @@ export default function Header() {
           className="flex-1 bg-transparent outline-none text-gray-400 placeholder-zinc-400 text-[16px] font-medium"
         />
 
-        {/* Шестёрёнка справа */}
+        {/* Шестерёнка справа */}
         <IconButton icon={<NutHex />} />
       </div>
     </div>
