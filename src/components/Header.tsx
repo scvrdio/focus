c50129d44303d3a9a70d9a2ede874ccb497ccd8b
serezha <scvrdio@gmail.com>
2025-04-10 22:08:50 +0300

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import { Magnifier, NutHex } from '@gravity-ui/icons';
 
 export default function Header() {
   return (
-    <div className="pt-safe-top">
+    <div className="pt-safe-top" style={{
+      paddingTop: 'calc(var(--tg-safe-area-inset-top, 0px) + var(--tg-content-safe-area-inset-top, 0px))',
+    }}>
       <div className="flex items-center rounded-xl px-6 py-2">
         {/* Лупа слева */}
         <Magnifier className="text-gray-400 mr-3" />
