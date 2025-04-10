1cd7d0481361e54054d21b9818ab9585833c4b69
serezha <scvrdio@gmail.com>
2025-04-10 22:00:59 +0300

    upd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,36 +13,37 @@ export default function Home() {
       <Header />
       {/* ShowCard сверху */}
       <main className="flex-1 overflow-y-auto no-scrollbar mb-0.5">
-        <ShowCard
-          image={
-            <img
-              src="/cards/poster.png"
-              alt="Poster"
-              className="w-full h-full object-cover"
-            />
-          }
-          season={1}
-          totalSeasons={3}
-          episode={2}
-          totalEpisodes={10}
-          onPosterClick={() => navigate('/content')}
-        />
+        <div className="flex flex-col gap-0.5">
+          <ShowCard
+            image={
+              <img
+                src="/cards/poster.png"
+                alt="Poster"
+                className="w-full h-full object-cover"
+              />
+            }
+            season={1}
+            totalSeasons={3}
+            episode={2}
+            totalEpisodes={10}
+            onPosterClick={() => navigate('/content')}
+          />
 
-        {/* Горизонтальная синяя карточка */}
-        <ListRow
-          image={
-            <img
-              src="/cards/now.png"
-              alt="Watching"
-              className="w-full h-auto"
-            />
-          }
-          title="Смотрю сейчас"
-          count={2}
-          size="sm"
-          align="center"
-          className="bg-[#2f8cff] text-white mb-4"
-        />
+          <ListRow
+            image={
+              <img
+                src="/cards/now.png"
+                alt="Watching"
+                className="w-full h-auto"
+              />
+            }
+            title="Смотрю сейчас"
+            count={2}
+            size="sm"
+            align="center"
+            className="bg-[#2f8cff] text-white mb-4"
+          />
+        </div>
 
         {/* Сетка из 2 карточек */}
         <div className="grid grid-cols-2 gap-4">
