f64cb4adc57bbd0ba99b0e4e5fc8ce763eb2483f
serezha <scvrdio@gmail.com>
2025-04-11 17:43:06 +0300

    upd

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -18,28 +18,33 @@ export default function ListCard({
   className = '',
 }: ListCardProps) {
   const colors = {
-    green: 'bg-black/80 text-green-300 ring-1 ring-green-400',
-    yellow: 'bg-black/80 text-indigo-300 ring-1 ring-indigo-400',
+    green: 'text-[#A2ADD0] bg-[#a2add0]',
+    yellow: 'text-[#AAD0A2] bg-[#aad0a2]',
   };
 
   return (
     <Squircle cornerRadius={20} borderWidth={1}>
-      <button
-        onClick={onClick}
-        className={clsx(
-          'flex flex-col justify-start relative rounded-[20px] aspect-[4/5] w-full overflow-hidden transition-all hover:ring-2 hover:scale-[1.02]',
-          colors[color],
-          className
-        )}
-      >
-        <div className="z-10">
-          <div className="text-[4vw] font-bold pt-4">{title}</div>
-        </div>
+      <div className={clsx('p-[1px]', colors[color])}>
+        <Squircle cornerRadius={19} borderWidth={1}>
+          <button
+            onClick={onClick}
+            className={clsx(
+              'flex flex-col justify-start relative rounded-[18px] aspect-[4/5] w-full overflow-hidden transition-all bg-black',
+              colors[color],
+              className
+            )}
+          >
+            <div className="z-10">
+              <div className="text-[4vw] font-black pt-4">{title}</div>
+            </div>
 
-        <div className="absolute bottom-[-35%] left-1/2 -translate-x-1/2 w-[155%] z-0 pointer-events-none">
-          {image}
-        </div>
-      </button>
+            <div className="absolute bottom-[-180%] left-1/2 -translate-x-1/2 w-[390%] z-0 pointer-events-none">
+              {image}
+            </div>
+
+          </button>
+        </Squircle>
+      </div>
     </Squircle>
   );
 
